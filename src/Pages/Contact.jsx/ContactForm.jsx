392096a3030726f442ec63ac2a2e6cd04ca9e5ef
Sangram Thapa <thapasangram64@gmail.com>
2025-01-17 13:17:21 +0545

    form added

--- src/Pages/Contact.jsx/ContactForm.jsx
+++ src/Pages/Contact.jsx/ContactForm.jsx
@@ -1,49 +1,61 @@
 import { useState } from "react";
 
 function ContactForm() {
-  const [name, setName] = useState(" ");
-  const [gender, setGender] = useState(" ");
-  const [error, setError] = useState(" ");
-
-  const submit = () => {
-    if (name === " ") {
-      setError("!name empty");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  function submit() {
+    if (name === "") {
+      setError("! Empty Name");
       return;
     }
-    if (gender === " ") {
-      setError("!gender not selected");
+    if (email === "") {
+      setError("! Empty Email");
+      return;
+    }
+    if (message === "") {
+      setError("! Empty text");
       return;
     }
 
-    console.log(name, gender);
-    setError(" ");
-    setName(" ");
-    setGender(" ");
-  };
+    setError("");
+    console.log(name);
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
   return (
     <>
-      <div style={{ color: "red" }}>{error}</div>
-      <label htmlFor="name">
-        Name:
+      <div style={{ color: "red", fontSize: "large" }}>{error}</div>
+      <div>
         <input
           type="text"
           onChange={(e) => setName(e.target.value)}
           value={name}
+          placeholder="Enter name"
         ></input>
-      </label>
-
-      <select
-        type="radio"
-        onChange={(e) => setGender(e.target.value)}
-        value={gender}
-      >
-        <option>Select</option>
-        <option>Male</option>
-        <option>Female</option>
-        <option>Other</option>
-      </select>
-
-      <input type="submit" onClick={submit}></input>
+      </div>
+      <div>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Enter email"
+        ></input>
+      </div>
+      <div>
+        <textarea
+          placeholder="message"
+          rows={5}
+          color="50"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        ></textarea>
+      </div>
+      <div>
+        <button onClick={submit}>Submit</button>
+      </div>
     </>
   );
 }
