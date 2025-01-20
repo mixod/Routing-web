81a0b4e3c626209d4cad3a94c3acad54b3ea317f
Sangram Thapa <thapasangram64@gmail.com>
2025-01-20 08:48:22 +0545

    data added

---
+++ src/assets/Data.js
@@ -0,0 +1,31 @@
+cons data = [
+    {
+        id: 4,
+        name: "diana",
+        age: 30,
+        email: "diana@example.com",
+        bio: "A graphic designer who enjoys art and creativity.",
+        location: "Berlin, Germany",
+        interests: ["art", "design", "movies"],
+    },
+    {
+        id: 5,
+        name: "emma",
+        age: 27,
+        email: "emma@example.com",
+        bio: "A fitness coach dedicated to helping people stay healthy.",
+        location: "Toronto, Canada",
+        interests: ["fitness", "cooking", "yoga"],
+    },
+    {
+        id: 6,
+        name: "farhan",
+        age: 35,
+        email: "farhan@example.com",
+        bio: "A data analyst with a love for numbers.",
+        location: "Mumbai, India",
+        interests: ["data science", "sports", "blogging"],
+    },
+]
+
+export { data };
