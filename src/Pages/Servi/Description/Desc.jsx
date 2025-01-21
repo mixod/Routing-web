2c5c7d04589369adabc73d1a908f19ff58f0d192
Sangram Thapa <thapasangram64@gmail.com>
2025-01-21 19:20:44 +0545

    data corrected

--- src/Pages/Servi/Description/Desc.jsx
+++ src/Pages/Servi/Description/Desc.jsx
@@ -1,24 +1,7 @@
-import { useParams } from "react-router";
-import { data } from "../../assets/Data";
-function Desc() {
-  const params = useParams();
-  if (isNaN(params.desc)) {
-    return <>Invalid params</>;
-  }
-  const id = parseInt(params.desc);
-  const userDatas = data.filter((item) => item.id === id);
-  if (userDatas.length <= 0) {
-    return <>Data not found</>;
-  }
+import React from "react";
 
-  const userData = userDatas[0];
-
-  return (
-    <div>
-      <div>name:{userData.name}</div>
-      <div>age:{userData.age}</div>
-    </div>
-  );
+function () {
+  return <div></div>;
 }
 
-export default Desc;
+export default s;
